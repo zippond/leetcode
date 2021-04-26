d2c2616029ceba44fb7f7001c63b196d3487d587
zippond <zhi-peng.dong@sap.com>
2021-04-26 11:14:00 +0800

    23 merge K lists. Loop method. Recursive TBD...

---
+++ src/23_merge_k_lists.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *mergeKLists2(vector<ListNode *> &lists)
+    {
+        if(lists.size() == 0) return nullptr;
+        return mergeKlistsLoop(lists);
+    }
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        if(lists.size() == 0) return nullptr;
+        mergeKListsRec(lists, 0, lists.size());
+        return lists[0];
+    }
+
+    void mergeKListsRec(vector<ListNode *> &lists, int start, int end)
+    {
+        if((end - start) == 1) { return;}
+        if((end - start) == 2) 
+        {
+            lists[start] = merge2Lists(lists[start], lists[end-1]);
+        }
+        int mid = start + (end - start)/2;
+        mergeKListsRec(lists, start, mid);
+        mergeKListsRec(lists, mid, end);
+    }
+    ListNode * mergeKlistsLoop(vector<ListNode *> &lists)
+    {
+        int len = 1;
+        int size = lists.size();
+        while(len < size)
+        {
+            for(int i = 0; (i + len)< size; i += 2* len)
+            {
+                lists[i] = merge2Lists(lists[i], lists[i+len]);
+            }
+            len *= 2;
+        }
+        return lists[0];
+    }
+
+    ListNode *merge2Lists(ListNode* l1, ListNode* l2)
+    {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        ListNode dummy(0);
+        ListNode * cur = &dummy;
+        while(l1&&l2)
+        {
+            if((l1->val) < (l2->val))
+            {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if(l1) cur->next = l1;
+        if(l2) cur->next = l2;
+        return dummy.next;
+    }
+    
+};
