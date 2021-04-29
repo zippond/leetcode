3fe6f34f1be635f64b10e3b4b633b8288bebccaf
zippond <zhi-peng.dong@sap.com>
2021-04-29 16:30:00 +0800

    Add iterative implementation.

--- src/25_reverse_nodes_in_k_groups.cpp
+++ src/25_reverse_nodes_in_k_groups.cpp
@@ -14,14 +14,53 @@ public:
         ListNode * next;
         while(a != b)
         {
+            // save
             next = a->next;
+            // update
             a->next = pre;
+            // reset
             pre = a;
             a = next;
         }
         return pre;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
+        if(k == 1) return head;
+        ListNode dummy;
+        ListNode * tail = &dummy;
+        ListNode * p1 = head;
+        ListNode * p2 = head;
+        bool reverseLast = false;
+
+        while(p2)
+        {          
+            int i = 0;
+            for(i = 0; i < k; i++)
+            {
+                p2 = p2->next;
+                if(!p2) 
+                {
+                    if(i == (k-1))
+                    {
+                        reverseLast = true;
+                    }
+                    break;
+                }
+                
+            }
+            if(p2||(reverseLast))
+            {
+                tail->next = reverse(p1, p2);
+            }
+            else{
+                tail->next = p1;
+            }
+            tail = p1;
+            p1 = p2;
+        }
+        return dummy.next;
+    }
+    ListNode* reverseKGroupRec(ListNode* head, int k) {
         if(k == 1) return head;
         ListNode *p1 = head;
         ListNode *p2 = head;
