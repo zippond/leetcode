be16f5d4aca286c7a09b1d2d80f8b5c0f4af7c8a
zippond <zhi-peng.dong@sap.com>
2021-05-25 08:40:28 +0800

    offer 02 get kth from end

---
+++ src/offer_02_kth_from_end.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode *p1 = head, *p2 = head;
+        for(int i = 0; i < k; i++)
+        {
+            if(!p1) return nullptr;
+            p1 = p1->next;
+        }
+        while(p1)
+        {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p2;
+    }
+};
