83ff8c0ecdd06bcb07b23a9648a98ed048d5113c
zippond <zhi-peng.dong@sap.com>
2021-05-28 15:23:30 +0800

    94 in order visit. iteratively.

---
+++ src/tree/94_in_order.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        vector<int> v;
+        stack<TreeNode*> s;
+        while(root || !s.empty())
+        {
+            while(root)
+            {
+                s.push(root);
+                root = root->left;
+            }
+            TreeNode *n = s.top();
+            v.push_back(n->val);
+            s.pop();
+            root = n->right;
+        }
+        return v;
+    }
+};
