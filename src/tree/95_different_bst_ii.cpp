ebf58cb7ac30a8b528146c57980abc66c1c68a57
zippond <zhi-peng.dong@sap.com>
2021-05-31 16:37:50 +0800

    95 different BST DP mode

--- src/tree/95_different_bst_ii.cpp
+++ src/tree/95_different_bst_ii.cpp
@@ -32,8 +32,43 @@ public:
         }
         return ret;
     }
-    vector<TreeNode*> generateTrees(int n) {
+    vector<TreeNode*> generateTrees2(int n) {
         if(!n) return {};
         return generateTrees(1, n);
+    }
+
+    vector<TreeNode*> generateTrees(int n)
+    {
+        vector<vector<vector<TreeNode *>> dp(n+1, vector<vector<TreeNode*>>(n+1));
+        for(int i = 1; i <= n; i++)
+        {
+            dp[i][i].push_back(new TreeNode(i));
+            dp[i][i-1].push_back(nullptr);
+        } 
+        for(int i = n-1; i > 0; i--)
+        {
+            for(int j = i+1;j<=n;j++)
+            {
+                //
+                vector<TreeNode *> v;
+                for(int k = i; k <= j; k++)
+                {
+                    vector<TreeNode *> left = dp[i][k-1];
+                    vector<TreeNode *> right = dp[k+1][j];
+                    for(auto l: left )
+                    {
+                        for(auto r: right)
+                        {
+                            TreeNode * node = new TreeNode(k);
+                            node->left = l;
+                            node->right = r;
+                            v.pus_back(node);
+                        }
+                    }
+                }
+                dp[i][j] = v;
+            }
+        }
+        return dp[1][n];
     }
 };
