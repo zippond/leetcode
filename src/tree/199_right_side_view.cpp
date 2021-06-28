79c490e1c8f5f94c1612a6ef97c0a6c1c4c794da
zippond <zhi-peng.dong@sap.com>
2021-06-28 08:58:45 +0800

    199 right side view. BFS to add last elem

---
+++ src/tree/199_right_side_view.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ret;
+        if(!root) return ret;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            for(int i = 0; i < size; i++)
+            {
+                TreeNode * node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+
+                if(i == (size - 1))
+                {
+                    ret.push_back(node->val);
+                }
+            }
+        }
+        return ret;
+    }
+};
