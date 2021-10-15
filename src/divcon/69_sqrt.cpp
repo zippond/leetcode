442cd1a2dbd381d4c03e0aa524843e46cf4c5913
zippond <zhi-peng.dong@sap.com>
2021-10-15 10:43:01 +0800

    69 sqare root

---
+++ src/divcon/69_sqrt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, h = x;
+        int m, ret;
+        while(l <= h) // 中止条件为 l > h
+        {           
+            m = l + (h - l)/2;
+            long long s = (long long)m*m;
+            if(s == x)
+            {
+                return m;
+            }
+            else if (s < x)
+            {
+                ret = m;
+                l = m+1;
+            }
+            else if(s > x)
+            {
+                h = m -1;
+            }
+        }
+        return ret;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    int n;
+    Solution s;
+    while(cin>>n)
+    {
+        cout<<s.mySqrt(n)<<endl;
+    }
+    return 0;
+}
