2d6f40c90b489c06290e618b2a136ef33e5dd7ef
zippond <zhi-peng.dong@sap.com>
2021-10-19 14:08:46 +0800

    Update sqare root method

--- src/divcon/69_sqrt.cpp
+++ src/divcon/69_sqrt.cpp
@@ -4,6 +4,33 @@ class Solution {
 public:
     int mySqrt(int x) {
         int l = 0, h = x;
+        int m;
+        while(l <= h) // 中止条件为 l > h
+        {           
+            m = l + (h - l)/2;
+            long long s = (long long)m*m;
+            if(s == x)
+            {
+                return m;
+            }
+            else if (s < x)
+            {
+                l = m+1;
+            }
+            else if(s > x)
+            {
+                h = m -1;
+            }
+        }
+        return h;
+    }
+};
+
+
+class Solution1 {
+public:
+    int mySqrt(int x) {
+        int l = 0, h = x;
         int m, ret;
         while(l <= h) // 中止条件为 l > h
         {           
@@ -26,7 +53,6 @@ public:
         return ret;
     }
 };
-
 int main(int argc, char **argv)
 {
     int n;
